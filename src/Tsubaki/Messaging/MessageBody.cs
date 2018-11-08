a488c9b78745750278389173f9788b74645d89f5
0x0001F36D <viyrex.aka.yuyu@gmail.com>
2018-11-08 08:53:00 +0800

    ^

--- src/Tsubaki/Messaging/MessageBody.cs
+++ src/Tsubaki/Messaging/MessageBody.cs
@@ -9,7 +9,7 @@ namespace Tsubaki.Messaging
         {
             if (string.IsNullOrWhiteSpace(message))
                 throw new ArgumentException("message can't be null or empty", nameof(message));
-            this.Payload = message;
+            this.Payload = message; 
         }
 
         internal string Payload { get; } 
